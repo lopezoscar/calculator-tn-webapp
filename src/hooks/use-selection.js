d39be6602ece18eb9f7094db27e3b9ad55db445e
Oscar López <oscar.lopez@alibrate.com>
2023-04-05 12:44:09 -0300

    remove sections

--- src/hooks/use-selection.js
+++ src/hooks/use-selection.js
@@ -1,29 +1,29 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react'
 
 export const useSelection = (items = []) => {
-  const [selected, setSelected] = useState([]);
+  const [selected, setSelected] = useState([])
 
   useEffect(() => {
-    setSelected([]);
-  }, [items]);
+    setSelected([])
+  }, [items])
 
   const handleSelectAll = useCallback(() => {
-    setSelected([...items]);
-  }, [items]);
+    setSelected([...items])
+  }, [items])
 
   const handleSelectOne = useCallback((item) => {
-    setSelected((prevState) => [...prevState, item]);
-  }, []);
+    setSelected((prevState) => [...prevState, item])
+  }, [])
 
   const handleDeselectAll = useCallback(() => {
-    setSelected([]);
-  }, []);
+    setSelected([])
+  }, [])
 
   const handleDeselectOne = useCallback((item) => {
     setSelected((prevState) => {
-      return prevState.filter((_item) => _item !== item);
-    });
-  }, []);
+      return prevState.filter((_item) => _item !== item)
+    })
+  }, [])
 
   return {
     handleDeselectAll,
@@ -31,5 +31,5 @@ export const useSelection = (items = []) => {
     handleSelectAll,
     handleSelectOne,
     selected
-  };
-};
+  }
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,27 @@
-import Head from 'next/head';
-import { CacheProvider } from '@emotion/react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context';
-import { useNProgress } from 'src/hooks/use-nprogress';
-import { createTheme } from 'src/theme';
-import { createEmotionCache } from 'src/utils/create-emotion-cache';
-import 'simplebar-react/dist/simplebar.min.css';
+import Head from 'next/head'
+import { CacheProvider } from '@emotion/react'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context'
+import { useNProgress } from 'src/hooks/use-nprogress'
+import { createTheme } from 'src/theme'
+import { createEmotionCache } from 'src/utils/create-emotion-cache'
+import 'simplebar-react/dist/simplebar.min.css'
 
-const clientSideEmotionCache = createEmotionCache();
+const clientSideEmotionCache = createEmotionCache()
 
-const SplashScreen = () => null;
+const SplashScreen = () => null
 
 const App = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
-  useNProgress();
+  useNProgress()
 
-  const getLayout = Component.getLayout ?? ((page) => page);
+  const getLayout = Component.getLayout ?? ((page) => page)
 
-  const theme = createTheme();
+  const theme = createTheme()
 
   return (
     <CacheProvider value={emotionCache}>
@@ -30,8 +30,8 @@ const App = (props) => {
           Devias Kit
         </title>
         <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
+          name='viewport'
+          content='initial-scale=1, width=device-width'
         />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -49,7 +49,7 @@ const App = (props) => {
         </AuthProvider>
       </LocalizationProvider>
     </CacheProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
